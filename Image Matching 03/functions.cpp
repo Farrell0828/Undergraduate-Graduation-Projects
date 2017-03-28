f19aad5e736eede2a5691848091d0d2ee75d1890
Farrell Meng <mengfanqi0828@163.com>
2017-03-28 19:25:32 +0800

    新增Show3DS( )、ShowOBJ( )两个函数，添加显示3ds模型和obj模型的功能。

--- Image Matching 03/functions.cpp
+++ Image Matching 03/functions.cpp
@@ -205,3 +205,82 @@ void ShowVideo(String file_name)
 	destroyWindow(file_name);
 	vid.release();
 }
+
+void Show3DS(const string &file_name)
+{
+	vtkSmartPointer<vtkRenderWindowInteractor> iren =
+		vtkSmartPointer<vtkRenderWindowInteractor>::New();	// 创建窗口交互器
+	vtkSmartPointer<vtkRenderWindow> renWin =
+		vtkSmartPointer<vtkRenderWindow>::New();			// 创建渲染窗口
+	vtkSmartPointer<vtkRenderer> renderer =
+		vtkSmartPointer<vtkRenderer>::New();				// 创建渲染器
+
+	iren->SetRenderWindow(renWin);		// 设置交互器控制的窗口为renWin
+	renWin->AddRenderer(renderer);		// 将渲染器renderer添加到窗口renWin的渲染器列表中
+
+	renderer->SetBackground(0.1, 0.2, 0.4);			// 设置背景色
+	renWin->SetSize(600, 600);			// 设置窗口的默认大小
+
+	vtkSmartPointer<vtk3DSImporter> importer =
+		vtkSmartPointer<vtk3DSImporter>::New();		// 创建3DS加载器对象
+	importer->SetFileName(file_name.c_str());		// 指定要读取的3DS文件的文件名或路径
+	importer->ComputeNormalsOn();		// 开启法向量计算
+	importer->Read();					// 加载3DS模型
+
+	renderer->GetActiveCamera()->SetPosition(100, 200, 0);	// 设置相机的位置(世界坐标系中)
+	renderer->GetActiveCamera()->SetFocalPoint(100, 0, 50);	// 设置相机的焦点(世界坐标系中)
+	renderer->GetActiveCamera()->SetViewUp(0, 0, 1);		// 设置相机的观察试图
+
+															// 获取加载的3DS模型中物体对象的个数
+	int numOfActors = importer->GetRenderer()->GetActors()->GetNumberOfItems();
+
+	// 从3ds图像中依次读取物体
+	for (int i = 0; i < numOfActors; i++)
+	{
+		vtkActor *actor = (vtkActor *)importer->GetRenderer()->GetActors()->GetItemAsObject(i);
+		renderer->AddActor(actor);		// 将上面获取的物体添加到渲染器中
+	}
+
+	renWin->Render();			// 令渲染窗口renWin中的所有渲染器开始渲染
+	iren->Start();				// 开始事件循环
+}
+
+void ShowOBJ(const string &file_name)
+{
+	vtkSmartPointer<vtkRenderWindowInteractor> iren =
+		vtkSmartPointer<vtkRenderWindowInteractor>::New();	// 创建窗口交互器
+	vtkSmartPointer<vtkRenderWindow> renWin =
+		vtkSmartPointer<vtkRenderWindow>::New();			// 创建渲染窗口
+	vtkSmartPointer<vtkRenderer> renderer =
+		vtkSmartPointer<vtkRenderer>::New();				// 创建渲染器
+
+	iren->SetRenderWindow(renWin);		// 设置交互器控制的窗口为renWin
+	renWin->AddRenderer(renderer);		// 将渲染器renderer添加到窗口renWin的渲染器列表中
+
+	renderer->SetBackground(0.1, 0.2, 0.4);			// 设置背景色
+	renWin->SetSize(600, 600);			// 设置窗口的默认大小
+	renWin->SetWindowName(file_name.c_str());
+
+	vtkSmartPointer<vtkOBJImporter> importer =
+		vtkSmartPointer<vtkOBJImporter>::New();		// 创建OBJ加载器对象
+	importer->SetFileName(std::string(file_name + ".obj").c_str());		// 指定要读取的OBJ文件
+	importer->SetFileNameMTL(std::string(file_name + ".mtl").c_str());	// 指定对应的MTL文件
+	importer->Read();					// 加载OBJ模型
+
+	renderer->GetActiveCamera()->SetPosition(100, 200, 0);	// 设置相机的位置(世界坐标系中)
+	renderer->GetActiveCamera()->SetFocalPoint(100, 0, 50);	// 设置相机的焦点(世界坐标系中)
+	renderer->GetActiveCamera()->SetViewUp(0, 0, 1);		// 设置相机的观察试图
+
+															// 获取加载的3DS模型中物体对象的个数
+	int numOfActors = importer->GetRenderer()->GetActors()->GetNumberOfItems();
+
+	// 从OBJ图像中依次读取物体
+	for (int i = 0; i < numOfActors; i++)
+	{
+		vtkActor *actor = (vtkActor *)importer->GetRenderer()->GetActors()->GetItemAsObject(i);
+		renderer->AddActor(actor);		// 将上面获取的物体添加到渲染器中
+	}
+
+	renWin->Render();			// 令渲染窗口renWin中的所有渲染器开始渲染
+	iren->Start();				// 开始事件循环
+}
--- Image Matching 03/functions.h
+++ Image Matching 03/functions.h
@@ -1,12 +1,28 @@
 #ifndef FUNCTIONS_H_
 
 #include <vector>
+#include <string>
+#include <windows.h>
 using namespace std;
 
+// OpenCV头文件
 #include <opencv2\opencv.hpp>
 #include <opencv2\xfeatures2d.hpp>
 using namespace cv;
 using namespace xfeatures2d;
+
+// VTK头文件
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkCamera.h>
+#include <vtk3DSImporter.h>
+#include <vtkOBJImporter.h>
+#include <vtkSmartPointer.h>
+#include <vtkRenderLargeImage.h>
+#include <vtkAutoInit.h>
+VTK_MODULE_INIT(vtkRenderingOpenGL2);
+VTK_MODULE_INIT(vtkInteractionStyle);
 
 /*
 @函数功能：对两张图片的特征点进行匹配，并返回经过距离和RANSAC筛选后较好的特征点的个数
@@ -38,5 +54,17 @@ void ShowResult(int no);
 @参数 file_name：需要展示的视频的路径或文件名
 */
 void ShowVideo(String file_name);
+
+/*
+@函数功能：显示3DS格式的3D模型
+@参数 file_name：需要展示的3DS模型文件的路径或文件名（包含扩展名.3ds）
+*/
+void Show3DS(const string &file_name);
+
+/*
+@函数功能：显示OBJ格式的3D模型
+@参数 file_name：需要展示的OBJ模型文件的路径或文件名（不包含扩展名.obj,相应的.mtl文件需和.obj文件同名且在同一目录）
+*/
+void ShowOBJ(const string &file_name);
 
 #endif // !FUNCTIONS_H_
